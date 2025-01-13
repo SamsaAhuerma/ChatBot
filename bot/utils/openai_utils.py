41625eca73f9acdc567589cf9ea53890c9e97f59
Samsa Ahuerma <106261599+SamsaAhuerma@users.noreply.github.com>
2025-01-13 12:02:09 -0300

    Update openai_utils.py

--- bot/utils/openai_utils.py
+++ bot/utils/openai_utils.py
@@ -16,8 +16,8 @@ def analyze_sentiment(text_s):
         model="gpt-4o-mini",
         messages=[
             {"role": "system", "content": "You are an assistant that responds only in English."},
-            {"role": "user", "content": f"Analyze the sentiment of the following text in Spanish and classify it as positive, neutral, or negative. Provide the classification, a brief justification, and a motivational suggestion. Ensure the response does not exceed 30 words, formatted in bullet points for clarity :\n\n{text_s}"}
-        ],Ensure the response does not exceed 30 words
+            {"role": "user", "content": f"Analyze the sentiment of the following text in Spanish and classify it as positive, neutral, or negative. Provide the classification, a brief justification, and a motivational suggestion. Ensure the response does not exceed 30 words, and format it in a simple, concise manner without bullet points or symbols:\n\n{text_s}"}
+        ],
         max_tokens=50
     )
     sentiment = response.choices[0].message.content
